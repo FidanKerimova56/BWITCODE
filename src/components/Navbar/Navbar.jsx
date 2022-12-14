33a660e1a9d1b8bbcbf6ebbfe49a6a909dae471e
FidanKerimova56 <Fidan.kerimova56@gmail.com>
2022-12-14 18:23:56 +0400

    nav is ready

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/logo.png";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-      
+      <nav className="nav">
+        <div className="logo-container">
+          <img src={logo} alt="" />
+        </div>
+        <div className="nav-links">
+          <ul>
+            <li>
+              <a href="">Portfolio</a>
+            </li>
+            <li>
+              <a href="">Services</a>
+            </li>
+            <li>
+              <a href="">About Us</a>
+            </li>
+          </ul>
+        </div>
+        <div className="nav-button">
+          <button>Project Estimation</button>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
