ded0ed3bb775b2ee3e162d5b23c2e60edf522d75
Fuad <fuadbayramv@gmail.com>
2023-01-04 20:14:05 +0400

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
-import "./Footer.css"
-import logo2 from "../../assets/logo2.png"
-import socialMedia from "../../assets/social-media.png"
+import React from "react";
+import "./Footer.css";
+import logo2 from "../../assets/logo2.png";
+import socialMedia from "../../assets/social-media.png";
 
 const Footer = () => {
   return (
-    <div className='footer-container'>
-        <h2>Ready to get started ?</h2>
-        <div className="nav">
-            <div className="logo">
-                <img src={logo2} alt="logo-footer" />
-            </div>
-            <div className="links">
-                <ul>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">About Us</a></li>
-                </ul>
-            </div>
-            <div className="social-media">
-                <img src={socialMedia} alt="socialMedia" />
-            </div>
+    <div className="footer-container">
+      <h2>Ready to get started ?</h2>
+      <div style={{ position: "relative" }}>
+        <div className="btn-container">
+          <button className="btn">Get in touch</button>
+          <button className="btn-border-secondary"></button>
+          <button className="btn-border-first"></button>
         </div>
-      
+      </div>
+      <div className="nav">
+        <div className="logo">
+          <img src={logo2} alt="logo-footer" />
+        </div>
+        <div className="links">
+          <ul>
+            <li>
+              <a href="#">Portfolio</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+          </ul>
+        </div>
+        <div className="social-media">
+          <img src={socialMedia} alt="socialMedia" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
